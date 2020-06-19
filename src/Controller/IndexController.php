4678e6be300a234567a158acd681578b6e87a870
fkropfhamer <fabian.kropfhamer@sblum.de>
2020-06-19 15:41:05 +0200

    first commit

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace App\Controller;
+
+
+
+use Symfony\Component\HttpFoundation\Response;
+
+class IndexController
+{
+    public function index(): Response
+    {
+        return new Response('Hello World!');
+    }
+}
